1cfbfa588511d13d75ac46915e9680c7043ed93d
Diego Santana <93400630+DiegosUx@users.noreply.github.com>
2022-05-24 19:28:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FuncionamentoElevador.classes;
+
+namespace FuncionamentoElevador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevador elevador = new Elevador();
+            Console.WriteLine("Bem vindo a Stark Tower, como você não é um vingador seu acesso esta liberado somente até o terceiro Andar.");
+            Console.WriteLine();
+            elevador.TotalDeAndares = 3;
+            elevador.Inicializa(elevador.Capacidade, elevador.TotalDeAndares);
+            bool opcao = true;
+           
+            do
+            {
+                Console.WriteLine("1 <- Adicionar Pessoa");
+                Console.WriteLine("2 <- Remover Pessoa");
+                Console.WriteLine("3 <- Subir um andar");
+                Console.WriteLine("4 <- Descer um andar");
+                Console.WriteLine("0 <- Sair");
+                string opcao1 = Console.ReadLine();
+                switch (opcao1)
+                {
+                    case "1":
+                        Console.WriteLine(elevador.Entrar(elevador.Capacidade));
+                        break;
+                    case "2":
+                        Console.WriteLine(elevador.Sair());
+                        break;
+                    case "3":
+                        Console.WriteLine(elevador.Subir(elevador.TotalDeAndares));
+                        break;
+                    case "4":
+                        Console.WriteLine(elevador.Descer(elevador.TotalDeAndares));
+                        break;
+                    case "0":
+                        Console.WriteLine("Obrigado por visitar a Stark Tower!");
+                        opcao = false;
+                        break;
+                    default:
+                        Console.WriteLine("Digite um valor valido");
+                        break;
+                }
+
+            } while (opcao != false);
+        }
+    }
+}
---
+++ elevador.cs
@@ -0,0 +1,92 @@
+﻿using System;namespace FuncionamentoElevador.classes
+{
+    public class Elevador
+    {
+        private int totalDeAndares = 3;
+        private int andarAtual = 0;
+        private int capacidade = 6;
+        private int pessoasPresentes;
+
+        public int Capacidade
+        {
+            get { return capacidade; }
+        }
+        public int TotalDeAndares
+        {
+            get { return totalDeAndares; }
+            set { totalDeAndares = value; }
+        }
+        public void Inicializa(int capacidade, int andares)
+        {
+            andarAtual = 0;
+            pessoasPresentes = 0;
+            Console.WriteLine($"Andar Atual é o Térreo e o elevador esta vazio");
+        }
+        public string Entrar(int capacidade)
+        {
+            if (pessoasPresentes < capacidade)
+            {
+                Console.WriteLine("Foi acrecentado uma pessoa");
+                this.pessoasPresentes = this.pessoasPresentes + 1;
+                return "O elevador tem " + pessoasPresentes + " pessoas";
+            }
+            else
+            {
+                return "Elevador está com a capacidade maxima de pessoas";
+            }
+        }
+        public string Sair()
+        {
+            if (pessoasPresentes != 0)
+            {
+                Console.WriteLine("Uma pessoa saiu do elevador");
+                this.pessoasPresentes = this.pessoasPresentes - 1;
+                return "O elevador tem " + pessoasPresentes + " pessoas";
+            }
+            else
+            {
+                return "Elevador já está vazio!";
+            }
+        }
+        public string Subir(int andares)
+        {
+            if (andarAtual < andares)
+            {
+                Console.WriteLine("Você subiu um andar");
+                this.andarAtual = this.andarAtual + 1;
+                if (this.andarAtual == 0)
+                {
+                    return "O elevador está no térreo";
+                }
+                else
+                {
+                    return "O elevador está no " + andarAtual + "º andar";
+                }
+            }
+            else
+            {
+                return "Você só tem acesso até o terceiro andar!!!";
+            }
+        }
+        public string Descer(int andares)
+        {
+            if (0 < andarAtual)
+            {
+                Console.WriteLine("Você desceu um andar");
+                this.andarAtual = this.andarAtual - 1;
+                if (this.andarAtual == 0)
+                {
+                    return "O elevador está no térreo";
+                }
+                else
+                {
+                    return "O elevador está no " + andarAtual + "º andar";
+                }
+            }
+            else
+            {
+                return "Não é possível descer mais um andar";
+            }
+        }
+    }
+}
